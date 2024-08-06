af29e03e8b52b022fab8d075bb9244a8774e51df
myosry91 <m.yosry91@yahoo.com>
2024-08-06 23:50:02 +0300

    fetch xml api2

--- src/Pages/StandAlone/XMLImport.js
+++ src/Pages/StandAlone/XMLImport.js
@@ -8,29 +8,41 @@ import SelectField from "../../Components/CustomForm/SelectField";
 import SuccessIcon from "../../Components/icons/SuccessIcon";
 import FailedIcon from "../../Components/icons/FailedIcon";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import LongArrow from "../../Components/icons/LongArrow";
+
+//test url: "https://convert.stockmount.com/xml/publish/28094/xmloutlet"
 
 const XMLImport = () => {
   // static data
   const response = "success";
-  const tableHeaders = [
-    { id: 1, header: "Header" },
-    { id: 2, header: "Sample Row" },
-    { id: 3, header: "Map To" },
+  const fields = [
+    { id: 1, name: "product" },
+    { id: 2, name: "product_sku" },
+    { id: 3, name: "price" },
+    { id: 1, name: "tax_percent" },
+    { id: 2, name: "barcode" },
+    { id: 3, name: "name" },
+    { id: 1, name: "description" },
+    { id: 2, name: "image" },
+    { id: 3, name: "stock" },
+    { id: 1, name: "category" },
+    { id: 2, name: "color" },
+    { id: 3, name: "size" },
   ];
   const data = [
     { id: 1, header: "product_sku", sample: "test", type: "number" },
     { id: 2, header: "category_name", sample: "test", type: "text" },
     { id: 3, header: "price", sample: "test", type: "number" },
   ];
-
   // states
-  const [createXmlData, setCreateXmlData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [activeIndex, setActiveIndex] = useState(0);
   const STAGES = XML_IMPORT_STEPS.map((step) => step.name);
   const [activeStage, setActiveStage] = useState(STAGES[0]);
   const [nextStep, setNextStep] = useState(false);
+  const [fieldsData, setFieldsData] = useState(null);
   const [fileData, setFileData] = useState({
     language_id: "",
     supplier_id: "",
@@ -38,43 +50,72 @@ const XMLImport = () => {
     template_name: "",
   });
 
+  // API URLs
+  const createXmlFileUrl =
+    process.env.REACT_APP_KADINLE_API + "xml-mapping/xml";
+  const getXmlFieldsUrl = process.env.REACT_APP_KADINLE_API + "xml-mapping/xml/9/fields"
+    
   // fetch data
-  const createXmlFileUrl = process.env.REACT_APP_KADINLE_API + "xml"
-  console.log(createXmlData);
-   // test button handler
-      const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true);
-        setError(null);
+  // 1.handle first step (create xml file)
+  const handleSubmitCreateXml = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(null);
 
-        const payload = {
-          url: "https://convert.stockmount.com/xml/publish/28094/xmloutlet",
-          template_name: "xml file1",
-        };
+    const payload = {
+      url: fileData.url,
+      template_name: fileData.template_name,
+    };
 
-        try {
-          const response = await fetch(createXmlFileUrl, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(payload),
-          });
+    try {
+      const response = await fetch(createXmlFileUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      console.log("response from server", response.status);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      if (response.status === 201) {
+        toast.success(`XML file created Successfully.`);
+        let index = STAGES?.indexOf(activeStage);
+        setActiveStage(STAGES?.[index + 1]);
+        setActiveIndex(index + 1);
+      } else {
+        toast.error("Error creating XML file.");
+      }
+    } catch (error) {
+      setError(error.toString());
+      toast.error("Error creating XML file.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-
-          const result = await response.json();
-          setCreateXmlData(result);
-          console.log(createXmlData);
-        } catch (error) {
-          setError(error.toString());
-          console.log("first catch", error);
-        } finally {
-          setLoading(false);
+  // 2.handle first step (get xml fields)
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(getXmlFieldsUrl);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-      };
+        const data = await response.json();
+        setFieldsData(data);
+      } catch (error) {
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    };
+     fetchData();
+  },[getXmlFieldsUrl])
+  console.log("fieldsData", fieldsData);
   // functions
   const checkFileData = (data) => {
     const isValid = Object.values(data).every((value) => value !== "");
@@ -85,19 +126,29 @@ const XMLImport = () => {
     checkFileData(fileData);
   }, [fileData]);
 
-  const onSubmit = () => {
-    let index = STAGES?.indexOf(activeStage);
-    setActiveStage(STAGES?.[index + 1]);
-    setActiveIndex(index + 1);
-  };
-
   return (
     <>
       <BlockPaper title="XML import">
         {/* STEPS */}
         <StepsBar items={XML_IMPORT_STEPS} activeIndex={activeIndex} />
+        <div className={`w-full flex justify-center items-center gap-20`}>
+          <div className="left flex flex-col gap-5">
+            {fields.map((field) => (
+              <div key={field.id} className="flex items-center justify-center gap-4">
+                <span className="w-20">{field.name}</span>
+                <div className="h-1 w-48 bg-[#5D9FF2] mt-2" />
+              </div>
+            ))}
+          </div>
+          <div className="center flex flex-col gap-5">
+            {/* {fields.map((field) => (
+              <div key={field.id} className="h-3 w-10 bg-[#5D9FF2]" />
+            ))} */}
+          </div>
+          <div className="right"></div>
+        </div>
         {/* BODY */}
-        <div className="my-10">
+        <div className="mt-10">
           {activeStage === STAGES[0] ? (
             <>
               <div className="flex flex-col md:flex-row w-full gap-5 mb-5">
@@ -154,51 +205,27 @@ const XMLImport = () => {
                   }}
                 />
               </div>
+              <div className="w-full flex justify-center">
+                <button
+                  disabled={!nextStep}
+                  className="p-2 my-8 rounded px-8 bg-primary-blue text-white disabled:bg-gray-200 disabled:text-gray-500"
+                  onClick={handleSubmitCreateXml}
+                >
+                  {loading ? "loading..." : "Submit"}
+                </button>
+              </div>
             </>
           ) : null}
 
           {activeStage === STAGES[1] ? (
-            <div className={`relative overflow-x-auto w-full`}>
-              <table className={`w-full`}>
-                <thead className={`bg-gray-100  text-sm`}>
-                  <tr>
-                    {tableHeaders.map((header) => (
-                      <th
-                        key={header.id}
-                        className={`text-gray-700 whitespace-nowrap  font-medium capitalize relative  group border-b border-gray-200  px-4 py-2 cursor-move`}
-                      >
-                        {header.header}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody className={``}>
-                  <>
-                    {data?.length ? (
-                      data.map((row) => {
-                        return (
-                          <tr
-                            key={row.id}
-                            className={`text-center border-b last:border-none even:bg-gray-100 border-gray-100 `}
-                          >
-                            <td className={`px-4 py-2`}>{row.header}</td>
-                            <td className={`px-4 py-2`}>{row.sample}</td>
-                            <td className={`px-4 py-2`}>{row.type}</td>
-                          </tr>
-                        );
-                      })
-                    ) : (
-                      <tr className="text-red-500 h-28 bg-[#f1f1f1e8] p-1 rounded-sm text-center mt-2">
-                        <td className="ltr:text-left rtl:text-right relative">
-                          <span className="sticky left-1/2 -translate-x-1/2">
-                            empty result
-                          </span>
-                        </td>
-                      </tr>
-                    )}
-                  </>
-                </tbody>
-              </table>
+            <div className={`w-full`}>
+              <div className="left">
+                {fields.map((field) => (
+                  <div key={field.id}>{field.name}</div>
+                ))}
+              </div>
+              <div className="center"></div>
+              <div className="right"></div>
             </div>
           ) : null}
           {activeStage === STAGES[2] ? (
@@ -242,8 +269,8 @@ const XMLImport = () => {
         </div>
       </BlockPaper>
       <div className="flex mt-12 gap-5 items-center justify-between">
-        <button onClick={handleSubmit}>test api</button>
-        {activeStage === STAGES?.[3] ? null : (
+        {/* <button onClick={handleSubmit}>test api</button> */}
+        {/* {activeStage === STAGES?.[3] ? null : (
           <button
             disabled={activeStage === STAGES[0]}
             className="p-2 rounded px-8 bg-primary-blue text-white disabled:bg-gray-200 disabled:text-gray-500"
@@ -278,7 +305,7 @@ const XMLImport = () => {
           >
             Next
           </button>
-        )}
+        )} */}
       </div>
     </>
   );

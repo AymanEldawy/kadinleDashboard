26aa9e890288c38ac88b7bd9a8fba2a19a766a87
myosry91 <m.yosry91@yahoo.com>
2024-08-07 19:28:14 +0300

    fetch xml api5

--- src/Pages/StandAlone/XMLImport.js
+++ src/Pages/StandAlone/XMLImport.js
@@ -9,7 +9,8 @@ import SuccessIcon from "../../Components/icons/SuccessIcon";
 import FailedIcon from "../../Components/icons/FailedIcon";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
-import {Reorder} from "framer-motion"
+import { Reorder } from "framer-motion";
+import Loading from "../../Components/Loading/Loading";
 
 //test url: "https://convert.stockmount.com/xml/publish/28094/xmloutlet"
 
@@ -17,21 +18,20 @@ const XMLImport = () => {
   // static data
   const response = "success";
   const fields = [
-    { id: 1, name: "product" },
-    { id: 2, name: "product_sku" },
-    { id: 3, name: "price" },
-    { id: 1, name: "tax_percent" },
-    { id: 2, name: "barcode" },
-    { id: 3, name: "name" },
-    { id: 1, name: "description" },
-    { id: 2, name: "image" },
-    { id: 3, name: "stock" },
-    { id: 1, name: "category" },
-    { id: 2, name: "color" },
-    { id: 3, name: "size" },
+    { id: 0, name: "product" },
+    { id: 1, name: "product_sku" },
+    { id: 2, name: "price" },
+    { id: 3, name: "tax_percent" },
+    { id: 4, name: "barcode" },
+    { id: 5, name: "name" },
+    { id: 6, name: "description" },
+    { id: 7, name: "image" },
+    { id: 8, name: "stock" },
+    { id: 9, name: "category" },
+    { id: 10, name: "color" },
+    { id: 11, name: "size" },
   ];
-  const [data,setData]= useState([1,2,3,4])
-  console.log("data",data)
+
   // states
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -49,9 +49,10 @@ const XMLImport = () => {
 
   // API URLs
   const createXmlFileUrl =
-    process.env.REACT_APP_KADINLE_API + "xml-mapping/xml";
-  const getXmlFieldsUrl = process.env.REACT_APP_KADINLE_API + "xml-mapping/xml/9/fields"
-    
+    process.env.REACT_APP_KADINLE_API + "/xml-mapping/xml";
+  const getXmlFieldsUrl =
+    process.env.REACT_APP_KADINLE_API + "/xml-mapping/xml/9/fields";
+
   // fetch data
   // 1.handle first step (create xml file)
   const handleSubmitCreateXml = async (event) => {
@@ -89,36 +90,102 @@ const XMLImport = () => {
       toast.error("Error creating XML file.");
     } finally {
       setLoading(false);
+      fetchData();
     }
   };
 
   // 2.handle first step (get xml fields)
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(getXmlFieldsUrl);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setFieldsData(data);
-      } catch (error) {
-        setError(error.toString());
-      } finally {
-        setLoading(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(getXmlFieldsUrl);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
+      const data = await response.json();
+      setFieldsData(data);
+    } catch (error) {
+      setError(error.toString());
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // 3.handle first step (create xml file)
+  const handleSubmitXmlFields = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    const payload = {
+      fields: {
+        product: fieldsData[0].key,
+        product_sku: fieldsData[1].key, //required text
+        price: fieldsData[2].key, //required double
+        tax_percent: fieldsData[3].key,
+        // "discount": "discount",
+        barcode: fieldsData[4].key,
+        name: fieldsData[5].key, //requried
+        description: fieldsData[6].key,
+        // "seo_title": "seo_title",
+        // "seo_description": "seo_description",
+        // "image_alt": "image_alt",
+        image: fieldsData[7].key, //required
+        // "weight": "weight",
+        // "sku": "sku",
+        // "pattern_sku": "pattern_sku",
+        stock: fieldsData[8].key, //requried
+        category: fieldsData[9].key, //required
+        color: fieldsData[10].key, //required
+        size: fieldsData[11].key, //required
+      },
     };
-     fetchData();
-  },[getXmlFieldsUrl])
-  console.log("fieldsData", fieldsData);
+
+    try {
+      const response = await fetch(getXmlFieldsUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      console.log("response from server", response);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      if (response.ok === true) {
+        toast.success(`Fields uploaded Successfully.`);
+        let index = STAGES?.indexOf(activeStage);
+        setActiveStage(STAGES?.[index + 1]);
+        setActiveIndex(index + 1);
+      } else {
+        toast.error("Error creating XML file.");
+      }
+    } catch (error) {
+      setError(error.toString());
+      toast.error("Error creating XML file.");
+    } finally {
+      setLoading(false);
+    }
+  };
   // functions
   const checkFileData = (data) => {
     const isValid = Object.values(data).every((value) => value !== "");
     setNextStep(isValid);
   };
 
+  const handleReorder = (newOrder) => {
+    const reorderedFields = newOrder.map((key) =>
+      fieldsData.find((field) => field.key === key)
+    );
+    setFieldsData(reorderedFields);
+  };
+
+  const handleRemoveField = (key) => {
+    setFieldsData(fieldsData.filter((field) => field.key !== key));
+  };
   useEffect(() => {
     checkFileData(fileData);
   }, [fileData]);
@@ -128,38 +195,7 @@ const XMLImport = () => {
       <BlockPaper title="XML import">
         {/* STEPS */}
         <StepsBar items={XML_IMPORT_STEPS} activeIndex={activeIndex} />
-        <h2 className="text-center font-semibold">
-          Drag and drop fields on the blue line
-        </h2>
-        <div className={`w-full flex justify-center items-center gap-20`}>
-          <div className="left flex flex-col gap-16">
-            {fields.map((field, index) => (
-              <div
-                key={field.id}
-                className="flex items-center justify-center gap-4"
-              >
-                <span className="w-20">{field.name}</span>
-                <div className="h-1 w-48 bg-[#5D9FF2] mt-2" />
-              </div>
-            ))}
-          </div>
 
-          <div className="right">
-            <Reorder.Group values={data} onReorder={setData}>
-              {data.map((field) => (
-                <Reorder.Item value={field} key={field}>
-                  <div
-                    key={field.id}
-                    className="p-4 my-1 rounded-md bg-[#5D9FF2] text-white"
-                  >
-                    {field}
-                  </div>
-                </Reorder.Item>
-              ))}
-            </Reorder.Group>
-          </div>
-        </div>
-        {/* <Draggable fields={fields} data ={data}/> */}
         {/* BODY */}
         <div className="mt-10">
           {activeStage === STAGES[0] ? (
@@ -224,7 +260,7 @@ const XMLImport = () => {
                   className="p-2 my-8 rounded px-8 bg-primary-blue text-white disabled:bg-gray-200 disabled:text-gray-500"
                   onClick={handleSubmitCreateXml}
                 >
-                  {loading ? "loading..." : "Submit"}
+                  {loading ? <Loading /> : "Submit"}
                 </button>
               </div>
             </>
@@ -232,13 +268,65 @@ const XMLImport = () => {
 
           {activeStage === STAGES[1] ? (
             <div className={`w-full`}>
-              <div className="left">
-                {fields.map((field) => (
-                  <div key={field.id}>{field.name}</div>
-                ))}
+              <h2 className="text-center font-semibold my-5">
+                Please arrange the items on the right
+              </h2>
+
+              {loading ? (
+                <div className="flex justify-center items-center">
+                  <Loading />
+                </div>
+              ) : (
+                <div className={`w-full flex justify-center`}>
+                  <div className="left flex flex-col gap-3 mt-1">
+                    {fields.map((field, index) => (
+                      <div
+                        key={field.id}
+                        className="flex items-center justify-center"
+                      >
+                        <span className="w-20">{field.name}</span>
+                        <div className="h-1 w-48 bg-[#E5E7EB] mt-2 ml-3" />
+                        <span className="text-[#5D9FF2] text-[22px] mt-1">
+                          ➤
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="right">
+                    {fieldsData?.length > 0 ? (
+                      <Reorder.Group
+                        values={fieldsData.map((field) => field.key)}
+                        onReorder={handleReorder}
+                      >
+                        {fieldsData?.map((field, index) => (
+                          <Reorder.Item value={field.key} key={field.key}>
+                            <div className="relative p-2 my-2 rounded-md bg-gray-600 text-white">
+                              {field.value}
+                              <span className="absolute right-2 top-0 rounded-full">
+                                <button
+                                  onClick={() => handleRemoveField(field.key)}
+                                  className="delete-button"
+                                >
+                                  ×
+                                </button>
+                              </span>
+                            </div>
+                          </Reorder.Item>
+                        ))}
+                      </Reorder.Group>
+                    ) : null}
+                  </div>
+                </div>
+              )}
+              <div className="w-full flex justify-center">
+                <button
+                  className="p-2 my-8 rounded px-8 bg-primary-blue text-white disabled:bg-gray-200 disabled:text-gray-500"
+                  onClick={handleSubmitXmlFields}
+                >
+                  {loading ? <Loading /> : "Submit"}
+                </button>
               </div>
-              <div className="center"></div>
-              <div className="right"></div>
             </div>
           ) : null}
           {activeStage === STAGES[2] ? (

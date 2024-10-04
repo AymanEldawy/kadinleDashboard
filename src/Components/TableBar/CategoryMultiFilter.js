d4a0f42d5b8da68224df4c4490b79e94f2bae537
Ayman Eldawy <aymaneldawy@yahoo.com>
2024-10-04 19:12:33 +0300

    modify filter

--- src/Components/TableBar/CategoryMultiFilter.js
+++ src/Components/TableBar/CategoryMultiFilter.js
@@ -41,7 +41,7 @@ export const CategoryMultiFilter = ({
   });
 
   return (
-    <div className="flex items-center gap-4">
+    <div className="flex items-center gap-4 overflow-hidden">
       {Object.entries(listCategories)?.map(([key, value]) => {
         return (
           <Select

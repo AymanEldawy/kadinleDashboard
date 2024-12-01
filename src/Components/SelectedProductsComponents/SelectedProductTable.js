c9049032624c5bea208c8c75c462d5dbc6de8312
Ayman Eldawy <aymaneldawy@yahoo.com>
2024-12-01 13:06:49 +0200

    modify offer products

--- src/Components/SelectedProductsComponents/SelectedProductTable.js
+++ src/Components/SelectedProductsComponents/SelectedProductTable.js
@@ -101,7 +101,7 @@ export const SelectedProductTable = ({
         param_lang_id: defaultLanguage?.id,
         param_products_ids: open ? Object.keys(rowSelection) : null,
         param_category_id: filter,
-        param_limit: 11,
+        param_limit: pagination?.pageSize,
         param_offset: pagination?.pageIndex,
         param_ignore_ids,
         param_price,
